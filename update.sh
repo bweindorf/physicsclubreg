54d8549fabcc65f429da6d8826fe71decef790c8
Brandon Weindorf <bw1326@messiah.edu>
2019-08-30 05:57:02 +0100

    update

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 cp /root/git/physicsclubreg/index.html /var/www/html/physicsclub/index.html
 cp /root/git/physicsclubreg/adder.php  /var/www/html/physicsclub/adder.php
+cp -r /root/git/physicsclubreg/gifs/*          /var/gifs/
+cp /root/git/physicsclubreg/admin.php  /var/www/html/physicsclub/admin.php
